0942c78ff79261db4621bd611d99fda171e2cd54
haufhun <haufhun@HUNTER_PC>
2017-03-09 20:53:02 -0600

    Implimented gainslosses inside the account and the portfolio.

--- Solution PA-2/Ticker501-MVC/Model/Account.cs
+++ Solution PA-2/Ticker501-MVC/Model/Account.cs
@@ -15,6 +15,10 @@ namespace Ticker501_MVC.Model
         public const decimal TRANSFER_FEE = 4.99m;
 
         /// <summary>
+        /// An instance of the database.
+        /// </summary>
+        private IDatabase _database;
+        /// <summary>
         /// The total cash in the account.
         /// </summary>
         public decimal CashBalance { get; set; }
@@ -26,27 +30,49 @@ namespace Ticker501_MVC.Model
         /// The total number of stocks invested.
         /// </summary>
         public int NumberOfStocks { get; set; }
+
         /// <summary>
         /// A field indicating how much the user has gained/lost.
         /// </summary>
-        public decimal GainsLosses { get; set; }
+        public decimal GainsLosses
+        {
+            get
+            {
+                var total = 0m;
+                foreach (var p in Portfolios.Values)
+                {
+                    foreach (var s in p.Stocks.Values)
+                    {
+                        total += (_database.StockDatabase[s.Name].Item3 * s.NumberOfShares) - s.InvestedBalance;
+                    }
+                }
+
+                return total - Fees;
+            }
+        }
         /// <summary>
         /// A dictionary of the portfolios associated with this account.
         /// </summary>
         public Dictionary<string, IPortfolio> Portfolios { get; set; }
-        
+
+        /// <summary>
+        /// A calculation of all the trade fees in the account.
+        /// </summary>
+        public decimal Fees { get; set; }
 
         /// <summary>
         /// Constructor initializing all fields. 
         /// </summary>
-        public Account()
+        public Account(IDatabase database)
         {
+            _database = database;
             CashBalance = 0m;
             InvestedBalance = 0m;
             NumberOfStocks = 0;
+            Fees = 0m;
             Portfolios = new Dictionary<string, IPortfolio>();
-            GainsLosses = 0m;
         }
+
         
     }
 }
--- Solution PA-2/Ticker501-MVC/Model/Portfolio.cs
+++ Solution PA-2/Ticker501-MVC/Model/Portfolio.cs
@@ -1,20 +1,32 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Ticker501_MVC.Model.Interfaces;
 
 namespace Ticker501_MVC.Model
 {
     public class Portfolio : IPortfolio
     {
+        
         public decimal InvestedBalance { get; set; }
         public int NumberOfStocks { get; set; }
         public Dictionary<string, IStock> Stocks { get; set; }
-        public decimal GainsLosses { get; set; }
+
+        private readonly IDatabase _database;
+
+        public decimal GainsLosses
+        {
+            get
+            {
+                return Stocks.Values.Sum(s => (_database.StockDatabase[s.Name].Item3 * s.NumberOfShares) - s.InvestedBalance);
+            }
+        }
 
         /// <summary>
         /// Constructor that initializes all components of the portfolio.
         /// </summary>
-        public Portfolio()
+        public Portfolio(IDatabase database)
         {
+            _database = database;
             InvestedBalance = 0m;
             NumberOfStocks = 0;
             Stocks = new Dictionary<string, IStock>();
