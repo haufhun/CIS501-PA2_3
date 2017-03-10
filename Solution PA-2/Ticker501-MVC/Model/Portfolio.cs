3e27568ad7b2f1616cd09f7fab5001b4f90033ad
haufhun <haufhun@HUNTER_PC>
2017-03-09 21:01:34 -0600

    Comment portfolio

--- Solution PA-2/Ticker501-MVC/Model/Portfolio.cs
+++ Solution PA-2/Ticker501-MVC/Model/Portfolio.cs
@@ -6,13 +6,23 @@ namespace Ticker501_MVC.Model
 {
     public class Portfolio : IPortfolio
     {
-        
+        /// <summary>
+        /// The amount of money invested in the portfolio
+        /// </summary>
         public decimal InvestedBalance { get; set; }
+        /// <summary>
+        /// The total number of stocks in the portfolio
+        /// </summary>
         public int NumberOfStocks { get; set; }
+        /// <summary>
+        /// A dictionary with a key of the stock name and a value of  
+        /// the associated stock class.
+        /// </summary>
         public Dictionary<string, IStock> Stocks { get; set; }
-
-        private readonly IDatabase _database;
-
+        /// <summary>
+        /// The value of the stocks at the current price 
+        /// minus the value of the stocks at their bought price.
+        /// </summary>
         public decimal GainsLosses
         {
             get
@@ -20,6 +30,13 @@ namespace Ticker501_MVC.Model
                 return Stocks.Values.Sum(s => (_database.StockDatabase[s.Name].Item3 * s.NumberOfShares) - s.InvestedBalance);
             }
         }
+        
+        /// <summary>
+        /// An instance of the database.
+        /// </summary>
+        private readonly IDatabase _database;
+
+
 
         /// <summary>
         /// Constructor that initializes all components of the portfolio.
